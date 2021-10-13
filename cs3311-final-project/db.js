0c07b1f5fb0d40d2d9505a8b90730f287ef70a1e
trainingmontage <nemo.omen@gmail.com>
2021-10-13 16:13:14 -0500

    Meh

--- cs3311-final-project/db.js
+++ cs3311-final-project/db.js
@@ -1,15 +1,17 @@
-import mysql from 'mysql2-promise';
-import dbConfig from './config/db.config.js';
+import mysql from "mysql2-promise";
+import dbConfig from "./config/db.config.js";
 
-const {host, user, password, database} = dbConfig;
+const { host, user, password, database } = dbConfig;
 
 const connection = mysql
-  .createConnection({host, user, password})
+  .createConnection({ host, user, password })
   .then((connection) => {
     connection.query(`CREATE DATABASE IF NOT EXISTS ${database}`);
   });
 
 connection.connect((error) => {
-  if(error) throw error;
-    console.log(`Connected to database ${dbConfig.database} at ${dbConfig.host}`);
+  if (error) throw error;
+  console.log(`Connected to database ${dbConfig.database} at ${dbConfig.host}`);
 });
+
+export default connection;
