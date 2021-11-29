8edaab130267b5a891c5c4d85837b6259c95b278
trainingmontage <nemo.omen@gmail.com>
2021-11-28 21:04:29 -0600

    Move .gitignore to root

--- final-project/recipe-app/main.cjs
+++ final-project/recipe-app/main.cjs
@@ -1,10 +1,21 @@
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  require('electron-reload')(__dirname, {
+      electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+      hardResetMethod: 'exit'
+  });
+}
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: true,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: './preload.js'
       // preload: path.join(__dirname, 'preload.js')
@@ -12,6 +23,8 @@ const createWindow = () => {
   });
 
   win.loadFile('render/index.html');
+
+  win.webContents.openDevTools();
 };
 
 app.whenReady().then(() => {
